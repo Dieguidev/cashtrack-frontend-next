05eafa078cea63a57c65c10e6e705fe4f0cbc7f4
Dieguidev <diegogaraycullas@gmail.com>
2025-01-26 18:03:40 -0500

    Añadir enlaces de navegación para recuperar contraseña en las páginas de inicio de sesión y registro, y corregir un error tipográfico en el texto.

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,5 +1,6 @@
 import { ForgotPasswordForm } from "@/components/auth/ForgotPasswordForm";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "CashTracker - Recuperar contraseña",
@@ -13,6 +14,11 @@ export default function ForgotPasswordPage() {
       <p className="text-3xl font-bold">Aquí puedes <span className="text-amber-500">reestablecerla</span></p>
 
       <ForgotPasswordForm />
+
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link href={'/auth/register'} className="text-center text-gray-500">¿No tienes cuenta? Crea una</Link>
+        <Link href={'/auth/login'} className="text-center text-gray-500">¿Ya tienes cuenta? Iniciar Sesión</Link>
+      </nav>
     </>
   );
 }
