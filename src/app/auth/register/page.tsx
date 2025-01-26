e11bdaec5ede564efddf512cbe42be6e9e3af81c
Dieguidev <diegogaraycullas@gmail.com>
2025-01-26 16:28:41 -0500

    agregar metadatos a la página de registro

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,4 +1,10 @@
 import { RegisterForm } from "@/components/auth/RegisterForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CashTracker - Crear Cuenta",
+  description: "CashTracker - Crear Cuenta",
+};
 
 export default function RegisterPage() {
   return (
