9636447e8076af6d8ab8654e2e5e0a224f5f9615
Dieguidev <diegogaraycullas@gmail.com>
2025-01-27 22:21:13 -0500

    Obteniendo los errores de validación

--- src/actions/create-account-action.ts
+++ src/actions/create-account-action.ts
@@ -1,14 +1,19 @@
-'use server'
+'use server';
 
-export async function register(formData: FormData){
+import { RegisterSchema } from '@/schemas';
 
+export async function register(formData: FormData) {
   const registerData = {
     email: formData.get('email'),
     name: formData.get('name'),
     password: formData.get('password'),
     password_confirmation: formData.get('password_confirmation'),
-  }
-  console.log(registerData);
+  };
 
+  const register = RegisterSchema.safeParse(registerData);
+
+  const errors = register.error?.errors.map((error) => error.message);
+
+  console.log(errors);
 
 }
