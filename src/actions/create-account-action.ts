eeee09b8b278412909db77e4425d5489367c4d83
Dieguidev <diegogaraycullas@gmail.com>
2025-01-28 16:29:42 -0500

    Enviando la petición con Fetch hacia el Backend

--- src/actions/create-account-action.ts
+++ src/actions/create-account-action.ts
@@ -11,11 +11,29 @@ export async function register(formData: FormData) {
   };
 
   const register = RegisterSchema.safeParse(registerData);
-
   const errors = register.error?.errors.map((error) => error.message);
 
-  console.log(errors);
+  if(!register.success) {
+    return {}
+  }
 
   const url = `${process.env.API_URL}/auth/register`;
+  const req = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: register.data.email,
+      name: register.data.name,
+      password: register.data.password,
+      passwordConfirmation: register.data.password
+    }),
+  })
+
+  const json = await req.json();
+
+  console.log(json);
+
 
 }
