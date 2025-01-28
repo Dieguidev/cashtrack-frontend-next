d0e014def7418421571574ef8f2393a5226c016a
Dieguidev <diegogaraycullas@gmail.com>
2025-01-27 22:07:02 -0500

    Creando un objeto para enviar hacia el backend

--- src/actions/create-account-action.ts
+++ src/actions/create-account-action.ts
@@ -1,6 +1,14 @@
 'use server'
 
 export async function register(formData: FormData){
-  console.log(formData.get('email'));
+
+  const registerData = {
+    email: formData.get('email'),
+    name: formData.get('name'),
+    password: formData.get('password'),
+    password_confirmation: formData.get('password_confirmation'),
+  }
+  console.log(registerData);
+
 
 }
