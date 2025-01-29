e59e4c0d8e996d6eab1ae2840bd7d2c9183aa18b
Dieguidev <diegogaraycullas@gmail.com>
2025-01-28 20:40:54 -0500

    Almacenando el Token en el State

--- src/components/auth/ConfirmAccountFrom.tsx
+++ src/components/auth/ConfirmAccountFrom.tsx
@@ -1,17 +1,33 @@
 'use client';
 
 import { PinInput, PinInputField } from "@chakra-ui/pin-input";
+import { useState } from "react";
 
 export const ConfirmAccountFrom = () => {
+
+  const [token, setToken] = useState('')
+
+  const handleChange = (token: string) => {
+    setToken(token)
+  }
+
+  const handleComplete = (token: string) => {
+
+  }
+
   return (
     <div className="flex justify-center gap-5 my-10">
-      <PinInput>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
-        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white"/>
+      <PinInput
+        value={token}
+        onChange={handleChange}
+        onComplete={handleComplete}
+      >
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
+        <PinInputField className="h-10 w-10 border-gray-300 shadow rounded-lg text-center placeholder-white" />
       </PinInput>
     </div>
   )
