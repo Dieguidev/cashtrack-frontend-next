34bb7e57daee653210e4e1613a7a2f1124860aa4
Dieguidev <diegogaraycullas@gmail.com>
2025-01-28 22:00:11 -0500

    Mejorando la legibilidad del código en la función de confirmación de cuenta

--- src/actions/confirm-account-action.ts
+++ src/actions/confirm-account-action.ts
@@ -1,15 +1,16 @@
-'use server'
+'use server';
 
 export type ActionStateType = {
   errors: string[];
-
 };
 
-export async function confirmAccount(token:string, prevState: ActionStateType){
-console.log(token);
+export async function confirmAccount(
+  token: string,
+  prevState: ActionStateType
+) {
+  console.log(token);
 
   return {
     errors: [],
-
-  }
+  };
 }
