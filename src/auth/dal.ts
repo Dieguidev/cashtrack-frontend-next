46396c7f74f7ed0ce9cd9818c7ed2132480faba8
Dieguidev <diegogaraycullas@gmail.com>
2025-01-30 22:19:42 -0500

    Obtener el JWT desde Cookies

--- src/auth/dal.ts
+++ src/auth/dal.ts
@@ -1,3 +1,13 @@
+
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+
 export const verifySession = async()=>{
+  const authCookie = await cookies();
+  const token = authCookie.get('CASHTRACKR_TOKEN')?.value;
+  if(!token) {
+    redirect('auth/login');
+  }
+
 
 }
