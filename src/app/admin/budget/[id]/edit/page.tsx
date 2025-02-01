f78866e0a05aa378d17d7169af729301c9feb715
Dieguidev <diegogaraycullas@gmail.com>
2025-02-01 09:30:14 -0500

    Consultar la API para obtener el presupuesto
    Añadiendo función para obtener datos del presupuesto al editar

--- src/app/admin/budget/[id]/edit/page.tsx
+++ src/app/admin/budget/[id]/edit/page.tsx
@@ -1,6 +1,26 @@
+import { getTokenFromCookies } from "@/auth/token";
+
+const getBudget = async (id: string) => {
+  const url = `${process.env.API_URL}/budget/${id}`;
+  const token = await getTokenFromCookies();
+  const req = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const json = await req.json();
+  if (!req.ok) {
+
+  }
+  return json;
+}
+
 export default async function EditBudgetPage({ params }: { params: { id: string } }) {
   const { id } = await params;
-  console.log(id);
+  getBudget(id);
 
   return (
     <div>EditBudgetPage</div>
