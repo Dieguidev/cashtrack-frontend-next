6d0b49228a6f867da4e76de2b33dd99ca54d75bc
Dieguidev <diegogaraycullas@gmail.com>
2025-02-01 09:49:01 -0500

    Añadiendo manejo de errores y página de no encontrado para la edición de presupuestos

--- src/app/admin/budget/[id]/edit/page.tsx
+++ src/app/admin/budget/[id]/edit/page.tsx
@@ -1,4 +1,5 @@
 import { getTokenFromCookies } from "@/auth/token";
+import { notFound } from "next/navigation";
 
 const getBudget = async (id: string) => {
   const url = `${process.env.API_URL}/budget/${id}`;
@@ -13,14 +14,16 @@ const getBudget = async (id: string) => {
 
   const json = await req.json();
   if (!req.ok) {
-
+    notFound();
   }
+
+
   return json;
 }
 
 export default async function EditBudgetPage({ params }: { params: { id: string } }) {
   const { id } = await params;
-  getBudget(id);
+  await getBudget(id);
 
   return (
     <div>EditBudgetPage</div>
