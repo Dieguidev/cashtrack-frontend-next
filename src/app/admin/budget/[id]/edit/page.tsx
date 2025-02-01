0003565e631236fb3982542fdf8a424bc8b65e64
Dieguidev <diegogaraycullas@gmail.com>
2025-02-01 10:28:08 -0500

    Validando el Presupuesto con un schema de ZOD y página de editar
    Mejorar la página de edición de presupuesto con validación de datos y diseño de interfaz

--- src/app/admin/budget/[id]/edit/page.tsx
+++ src/app/admin/budget/[id]/edit/page.tsx
@@ -1,4 +1,6 @@
 import { getTokenFromCookies } from "@/auth/token";
+import { BudgetAPIResponseSchema } from "@/schemas";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 const getBudget = async (id: string) => {
@@ -17,15 +19,36 @@ const getBudget = async (id: string) => {
     notFound();
   }
 
+  const budget = BudgetAPIResponseSchema.parse(json);
 
-  return json;
+  return budget;
 }
 
 export default async function EditBudgetPage({ params }: { params: { id: string } }) {
   const { id } = await params;
-  await getBudget(id);
+  const budget = await getBudget(id);
 
   return (
-    <div>EditBudgetPage</div>
+    <>
+      <div className='flex flex-col-reverse md:flex-row md:justify-between items-center'>
+        <div className='w-full md:w-auto'>
+          <h1 className='font-black text-4xl text-purple-950 my-5'>
+            Editar Presupuesto: {budget.name}
+          </h1>
+          <p className="text-xl font-bold">Llena el formulario y crea un nuevo {''}
+            <span className="text-amber-500">presupuesto</span>
+          </p>
+        </div>
+        <Link
+          href={'/admin'}
+          className='bg-amber-500 p-2 rounded-lg text-white font-bold w-full md:w-auto text-center'
+        >
+          Volver
+        </Link>
+      </div>
+      <div className='p-10 mt-10  shadow-lg border '>
+
+      </div>
+    </>
   );
 }
