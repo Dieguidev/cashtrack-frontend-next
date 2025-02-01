87bdef225ecfe0541fb3c67c2e2ce3db01b8ecfc
Dieguidev <diegogaraycullas@gmail.com>
2025-01-31 23:45:22 -0500

    Implementar formulario para crear presupuesto con validación y manejo de estado

--- src/components/budget/CreateBudgetForm.tsx
+++ src/components/budget/CreateBudgetForm.tsx
@@ -1,8 +1,61 @@
 "use client"
 
+import { ActionStateType } from "@/actions/confirm-account-action";
+import { createBudget } from "@/actions/create-budget-action";
+import { useRouter } from "next/navigation";
+import { useActionState, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
 export const CreateBudgetForm = () => {
+
+  const router = useRouter()
+
+  const [formData, setFormData] = useState({
+    amount: '',
+    name: '',
+  });
+
+  const [state, dispatch] = useActionState(async (prevState: ActionStateType, form: FormData) => {
+    const result = await createBudget(prevState, form);
+
+    if (result.success) {
+      setFormData({
+        amount: '',
+        name: '',
+      });
+    }
+
+    return result;
+  }, {
+    errors: [],
+    success: '',
+  });
+
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach(error => {
+        toast.error(error)
+      })
+    }
+    if (state.success) {
+      toast.success(state.success, {
+        onClose: () => {
+          router.push('/admin')
+        },
+        onClick: () => {
+          router.push('/admin')
+        }
+      })
+    }
+  }, [state, router])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <form
+      action={dispatch}
       className="mt-10 space-y-3"
       noValidate
     >
@@ -16,6 +69,8 @@ export const CreateBudgetForm = () => {
           type="text"
           placeholder="Nombre del Presupuesto"
           name="name"
+          value={formData.name}
+          onChange={handleChange}
         />
       </div>
       <div className="space-y-3">
@@ -28,6 +83,8 @@ export const CreateBudgetForm = () => {
           className="w-full p-3  border border-gray-100 bg-slate-100"
           placeholder="Cantidad Presupuesto"
           name="amount"
+          value={formData.amount}
+          onChange={handleChange}
         />
       </div>
       <input
