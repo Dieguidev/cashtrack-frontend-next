6230e8f1038d8ccc1ddc9dcecde0764627c896d4
Dieguidev <diegogaraycullas@gmail.com>
2025-02-02 16:28:09 -0500

    Mostrar y Ocutar Modal
    Agregar ModalContainer y corregir la navegación del botón de agregar gasto

--- src/app/admin/budget/[id]/page.tsx
+++ src/app/admin/budget/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { AddExpenseButton } from "@/components/expenses/AddExpenseButton";
+import { ModalContainer } from "@/components/ui/ModalContainer";
 import { getBudget } from "@/services/budgets";
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
@@ -15,12 +16,15 @@ export default async function BudgetDetailsPage({ params }: { params: { id: stri
   const budget = await getBudget(id);
 
   return (
-    <div className='flex justify-between items-center'>
-      <div>
-        <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
-        <p className="text-xl font-bold">Administra tus {''} <span className="text-amber-500">gastos</span></p>
+    <>
+      <div className='flex justify-between items-center'>
+        <div>
+          <h1 className="font-black text-4xl text-purple-950">{budget.name}</h1>
+          <p className="text-xl font-bold">Administra tus {''} <span className="text-amber-500">gastos</span></p>
+        </div>
+        <AddExpenseButton />
       </div>
-      <AddExpenseButton />
-    </div>
+      <ModalContainer />
+    </>
   );
 }
--- src/components/expenses/AddExpenseButton.tsx
+++ src/components/expenses/AddExpenseButton.tsx
@@ -10,7 +10,7 @@ export const AddExpenseButton = () => {
     <button
       type='button'
       className='bg-amber-500 px-10 py-2 rounded-lg text-white font-bold cursor-pointer'
-      onClick={()=> router.push('?addExpense=true&?showModal=true')}
+      onClick={()=> router.push('?addExpense=true&showModal=true')}
     >Agregar Gastoo</button>
   )
 }
