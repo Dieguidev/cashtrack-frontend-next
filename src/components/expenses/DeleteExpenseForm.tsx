efcc3ebbe2a33b3550ae7e1c3f2a3eb8a0d061b2
Dieguidev <diegogaraycullas@gmail.com>
2025-02-04 18:50:35 -0500

    Añadiendo funcionalidad para eliminar gastos con manejo de errores y notificaciones

--- src/components/expenses/DeleteExpenseForm.tsx
+++ src/components/expenses/DeleteExpenseForm.tsx
@@ -1,14 +1,43 @@
 import { useParams, useSearchParams } from "next/navigation";
 import { DialogTitle } from "@headlessui/react";
+import { deleteExpense } from "@/actions/delete-expense-action";
+import { startTransition, useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 type DeleteExpenseForm = {
   closeModal: () => void
 }
 
 export const DeleteExpenseForm = ({ closeModal }: DeleteExpenseForm) => {
-  const { id: budgetId } = useParams()
+  const { id: budgetId } = useParams<{ id: string }>()
   const searchParams = useSearchParams()
   const expenseId = searchParams.get('deleteExpenseId')!
+
+  const deleteExpenseWithIdAndBudgetId = deleteExpense.bind(null, { budgetId, expenseId })
+  const [state, dispatch] = useActionState(deleteExpenseWithIdAndBudgetId, {
+    errors: [],
+    success: '',
+  })
+
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach(error => {
+        toast.error(error)
+      })
+    }
+    if (state.success) {
+      toast.success(state.success)
+      closeModal();
+    }
+  }, [state, closeModal])
+
+
+  //* Esta funcion se encarga de eliminar el gasto,usando correctamente el hook useActionState en eventos del cliente
+  const handleClick = () => {
+    startTransition(() => {
+      dispatch();
+    });
+  };
 
   return (
     <>
@@ -32,6 +61,7 @@ export const DeleteExpenseForm = ({ closeModal }: DeleteExpenseForm) => {
           type='submit'
 
           className="bg-red-500 w-full p-3 text-white uppercase font-bold hover:bg-red-600 cursor-pointer transition-colors"
+          onClick={handleClick}
         >Eliminar</button>
       </div>
     </>
