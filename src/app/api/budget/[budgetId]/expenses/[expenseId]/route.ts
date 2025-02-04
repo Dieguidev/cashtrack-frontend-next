f587476a702f43c25d4ade549eda7e3ae9e451c4
Dieguidev <diegogaraycullas@gmail.com>
2025-02-03 22:38:59 -0500

    Consultando la API para obtener el presupuesto (API Route)
    Mejorar el endpoint GET para gastos: agregar verificación de sesión y manejo de errores con token de autorización

--- src/app/api/budget/[budgetId]/expenses/[expenseId]/route.ts
+++ src/app/api/budget/[budgetId]/expenses/[expenseId]/route.ts
@@ -1,7 +1,28 @@
-import { verifySession } from "@/auth/dal"
+import { verifySession } from '@/auth/dal';
+import { getTokenFromCookies } from '@/auth/token';
 
-export async function GET() {
+export async function GET(
+  request: Request,
+  { params }: { params: { budgetId: string; expenseId: string } }
+) {
+  await verifySession();
 
-  await verifySession()
-  return Response.json('hola mundo')
+  const token = getTokenFromCookies();
+
+  const url = `${process.env.API_URL}/expense/${params.expenseId}/budget/${params.budgetId}`;
+  const req = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const json = await req.json();
+
+  if(!req.ok){
+    return Response.json(json.errors, { status: req.status });
+  }
+
+  return Response.json(json);
 }
