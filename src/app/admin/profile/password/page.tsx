f058bc1aab6611634be438ebd4a69f89d2c2f030
Dieguidev <diegogaraycullas@gmail.com>
2025-02-04 22:55:25 -0500

    Añadiendo páginas de perfil, cambiar contraseña y configuración, junto con el componente de pestañas de perfil

---
+++ src/app/admin/profile/password/page.tsx
@@ -0,0 +1,10 @@
+export default function ChangePasswordPage() {
+  return (
+    <>
+      <h1 className="font-black text-4xl text-purple-950 my-5">Cambiar Password</h1>
+      <p className="text-xl font-bold">Aquí puedes cambiar tu {''}
+        <span className="text-amber-500">password</span>
+      </p>
+    </>
+  )
+}
