178499e199562cf46ed113f3c48fa4efa9d1b7ef
Dieguidev <diegogaraycullas@gmail.com>
2025-02-05 06:48:12 -0500

    Añadiendo componente de formulario de perfil para editar nombre y email

--- src/app/admin/profile/settings/page.tsx
+++ src/app/admin/profile/settings/page.tsx
@@ -1,3 +1,5 @@
+import { ProfileForm } from "@/components/profile/ProfileForm";
+
 export default function EditProfilePage() {
   return (
     <>
@@ -5,6 +7,9 @@ export default function EditProfilePage() {
         <p className="text-xl font-bold">Aquí puedes cambiar los datos de tu {''}
             <span className="text-amber-500">perfil</span>
         </p>
+
+        <ProfileForm />
     </>
+
   )
 }
